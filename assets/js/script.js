8a714530bed3a598ec9ac8278e6597f457dac034
Mystic Kitty <djabranton@gmail.com>
2020-10-06 12:00:51 -0700

    Added column highlighting to drag and drop feature.

--- assets/js/script.js
+++ assets/js/script.js
@@ -192,6 +192,7 @@ var dropZoneDragHandler = function (event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -212,7 +213,15 @@ var dropTaskHandler = function (event) {
         statusSelectEl.selectedIndex = 2;
     }
     dropZoneEl.appendChild(draggableElement);
+    dropZoneEl.removeAttribute("style");
 };
+
+var dragLeaveHandler = function (event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
@@ -223,5 +232,7 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
 
